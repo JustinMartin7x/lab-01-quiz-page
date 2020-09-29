39d96fd9f41378223f040f1a36b3b939c1f0bf13
JustinMartin7x <justin.martin7x@gmail.com>
2020-09-28 17:38:12 -0700

    Added second quiz

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ button.addEventListener('click', () => {
     endMessage.textContent = `congratulations ${userName} you met ${answersRight} conditions out of 4 for being a Super Saiyan!`;
 // console.log(answersRight);
 
-    if (answersRight === 3) {
+    if (answersRight === 4) {
         positiveResults.style.display = 'block';
         neutralResults.style.display = 'none';
     } else {
@@ -60,3 +60,75 @@ button.addEventListener('click', () => {
     } 
 });
 
+const knowlegeButton = document.getElementById ('knowlegeqQuizButton');
+const quizResults = document.getElementById('quizResults');
+
+
+knowlegeButton.addEventListener('click', () => {
+
+    let rightAnswers = 0;
+
+    const userName = prompt('What is your Name?');
+    const planet = prompt('what planet are you from?');
+    const species = prompt('what species are you?');
+    alert(`ok ${species} from planet ${planet} named ${userName} ! Prepare yourself!`);
+
+    const goku = prompt(`${species} from planet ${planet} named ${userName} Is Goku a native human from earth?`);
+
+    if (!countAsYes(goku)) {
+        
+        rightAnswers++;
+
+    }
+    const yamcha = prompt(`${species} from planet ${planet} named ${userName} is yamcha the weakes of the Z-Fighters?`);
+
+    if (countAsYes(yamcha)) {
+
+        rightAnswers++;
+
+    }
+
+    const gohan = prompt(`${species} from planet ${planet} named ${userName} is Gohan the son of Goku?`);
+
+    if (countAsYes(gohan)) {
+
+        rightAnswers++;
+
+    }
+
+    const vageta = prompt(`${species} from planet ${planet} named ${userName} is Vageta the prince of all Saiyans?`);
+
+    if (countAsYes(vageta)) {
+
+        rightAnswers++;
+
+    }
+
+    alert(`${species} from planet ${planet} named ${userName} You have answered all of the questions. Please wait while your results are tallied!`);
+    alert('your result will now be displayed on the page!');
+
+    if (rightAnswers === 0) {
+        quizResults.style.display = 'block';
+        quizResults.textContent = 'you answered 0 out of 4 correct!! you know nothing of DragonBall Z!!';
+    }
+    if (rightAnswers === 1) {
+        quizResults.style.display = 'block';
+        quizResults.textContent = 'you answered 1 out of 4 correct! you know nex to nothing about Dragon Ball Z';
+    }
+    if (rightAnswers === 2) {
+        quizResults.style.display = 'block';
+        quizResults.textContent = 'you answered 2 out of 4 correctly! you have a bit of knowlege about DragonBall Z!';
+    }
+    if (rightAnswers === 3) {
+        quizResults.style.display = 'block';
+        quizResults.textContent = 'you answered 3 out of 4 correctly! you have spent many hours watching DragonBall Z!!';
+        
+    }
+    console.log(quizResults);
+
+    if (rightAnswers === 4) {
+        quizResults.style.display = 'block';
+        quizResults.textContent = 'you answered 4 out of 4 correct!! congratulations you have no life!';
+    }
+    
+});
--- test/functions.js
+++ test/functions.js
@@ -10,3 +10,18 @@ export function powerLevel(userInput) {
     return userInput > 9000;
 
 }
+// export funtion quizCheck(score) {
+
+// if (score === 0) {
+//     return `Oops you got 0/3!`;
+// }
+// if (score === 1) {
+//     return `You got 1/3 you know a little but still more to learn`;
+// }
+// if (score === 2) {
+//     return `OoOOo, almost there! You got 2/3`;
+// }
+// if (score === 3) {
+//     return `Congrats! You got a perfect 3/3.`;
+// }
+// }
